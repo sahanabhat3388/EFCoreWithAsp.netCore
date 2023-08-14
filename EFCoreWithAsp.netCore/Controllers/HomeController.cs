a43cc80f76a83a5d51cf7d1dae43ae203297586c
Sahana Bhat <SahanaBhat3388@gmail.com>
2023-08-14 10:40:56 +0530

    Add project files.

---
+++ EFCoreWithAsp.netCore/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreWithAsp.netCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+       
+    }
+}
