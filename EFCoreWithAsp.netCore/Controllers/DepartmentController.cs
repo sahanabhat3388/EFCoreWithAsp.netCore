ab86d57b39f1226b614ae0946f829de48c27adc8
Sahana Bhat <SahanaBhat3388@gmail.com>
2023-08-31 16:21:58 +0530

    Designing add employee and add department forms

---
+++ EFCoreWithAsp.netCore/Controllers/DepartmentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreWithAsp.netCore.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ EFCoreWithAsp.netCore/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreWithAsp.netCore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
