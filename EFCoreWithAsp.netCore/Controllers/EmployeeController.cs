5e9907954454af54db3dacd390bee1a42516c3cd
Sahana Bhat <SahanaBhat3388@gmail.com>
2023-10-12 10:25:25 +0530

    Implemented Sorting

--- EFCoreWithAsp.netCore/Controllers/EmployeeController.cs
+++ EFCoreWithAsp.netCore/Controllers/EmployeeController.cs
@@ -16,14 +16,44 @@ namespace EFCoreWithAsp.netCore.Controllers
         {
             _employeeRepository = employeeRepository;
         }
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string searchString, string sortOrder)
         {
             var employees = await _employeeRepository.GetAllAsync();
 
+            // Search functionality
             if (!String.IsNullOrEmpty(searchString))
             {
                 employees = employees.Where(n => n.FirstName.Contains(searchString)
                 || n.LastName.Contains(searchString)).ToList();
+            }
+
+            ViewData["NameSortParam"] = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateOfBirthSortParm"] = sortOrder == "date_asc" ? "date_desc" : "date_asc";
+            ViewData["IsActiveSortParam"] = sortOrder == "isactive_asc" ? "isactive_desc" : "isactive_asc";
+
+            //Sorting
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    employees = employees.OrderByDescending(e => e.FirstName).ToList();
+                    break;
+
+                case "date_asc":
+                    employees = employees.OrderBy(s => s.DateOfBirth).ToList();
+                    break;
+                case "date_desc":
+                    employees = employees.OrderByDescending(s => s.DateOfBirth).ToList();
+                    break;
+                case "isactive_desc":
+                    employees = employees.OrderByDescending(e => e.IsActive).ToList();
+                    break;
+                case "isactive_asc":
+                    employees = employees.OrderBy(e => e.IsActive).ToList();
+                    break;
+
+                default:
+                    employees = employees.OrderBy(e => e.FirstName).ToList();
+                    break;
             }
 
             return View(employees);
